d251b3924f9c6fe97ec1f09a02d1bbe2db7a3aee
GGBOND <1454538956@qq.com>
2025-06-30 11:34:40 +0800

    优化zshrc适配不同的操作系统

--- config/.config/zsh/env.zsh
+++ config/.config/zsh/env.zsh
@@ -1,10 +1,27 @@
-export https_proxy=http://127.0.0.1:7890 
-export http_proxy=http://127.0.0.1:7890
-export all_proxy=socks5://127.0.0.1:7890
 export XDG_CONFIG_HOME=$HOME/.config
 export PATH=$HOME/.local/bin:$PATH
 export EDITOR=nvim
 # 终端输出中文
 export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
+
+
+export http_proxy="http://127.0.0.1:7890"
+export https_proxy="http://127.0.0.1:7890"
+export all_proxy=socks5://127.0.0.1:7890
+
+if [[ -f /etc/os-release ]]; then
+  source /etc/os-release
+  if [[ "$ID" = "ubuntu" ]]; then
+    # homebrew linux
+    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+    # clash
+    pgrep -x clash >/dev/null || ~/clash/clash -d ~/clash > /tmp/clash.log 2>&1 &
+    # Qt
+    export PATH=/home/ggbond/Qt/6.6.3/gcc_64/bin:$PATH
+    export LD_LIBRARY_PATH=/home/ggbond/Qt/6.6.3/gcc_64/lib:$LD_LIBRARY_PATH
+    export QML2_IMPORT_PATH=/home/ggbond/Qt/6.6.3/gcc_64/qml
+    export QT_PLUGIN_PATH=/home/ggbond/Qt/6.6.3/gcc_64/plugins
+  fi
+fi
 
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -3,22 +3,6 @@ source ~/.config/zsh/aliases.zsh
 source ~/.config/zsh/vi.zsh
 
 
-if [[ -f /etc/os-release ]]; then
-  source /etc/os-release
-  if [[ "$ID" = "ubuntu" ]]; then
-    # homebrew linux
-    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-    # clash
-    pgrep -x clash >/dev/null || ~/clash/clash -d ~/clash > /tmp/clash.log 2>&1 &
-    export http_proxy="http://127.0.0.1:7890"
-    export https_proxy="http://127.0.0.1:7890"
-    # Qt
-    export PATH=/home/ggbond/Qt/6.6.3/gcc_64/bin:$PATH
-    export LD_LIBRARY_PATH=/home/ggbond/Qt/6.6.3/gcc_64/lib:$LD_LIBRARY_PATH
-    export QML2_IMPORT_PATH=/home/ggbond/Qt/6.6.3/gcc_64/qml
-    export QT_PLUGIN_PATH=/home/ggbond/Qt/6.6.3/gcc_64/plugins
-  fi
-fi
 
 ZIM_HOME=${ZDOTDIR:-${HOME}}/.zim
 # Download zimfw plugin manager if missing.
